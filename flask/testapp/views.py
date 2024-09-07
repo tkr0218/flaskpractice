25c0e60e235601839320ea65751de5c64f39ad48
tkr0218 <218fuji@gmail.com>
2024-09-08 01:23:30 +0900

    add flask
    practice github

---
+++ flask/testapp/views.py
@@ -0,0 +1,24 @@
+from testapp import app
+from flask import render_template,Flask
+n=1000000
+s=[0 for i in range(n)]
+t=[]
+for i in range(2,n):
+    if s[i]==0:
+        t.append(i)
+        if i<1001:
+            for j in range(2*i,n,i):
+                s[j]=1
+@app.route('/')
+def index():
+    my_dict = {
+        'insert_something1': 'views.pyのinsert_something1部分です。',
+        'insert_something2': 'views.pyのinsert_something2部分です。',
+        'test_titles': t
+    }
+    return render_template('testapp/index.html',my_dict=my_dict)
+@app.route('/test')
+def other1():
+    return render_template('testapp/index2.html')
+#if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0', port=5000)
